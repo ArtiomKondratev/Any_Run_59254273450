f3d01fd5c343a58b8c1c907128a833ac5972e6df
ArtiomKondratev <artem-kondratev-1980@mail.ru>
2023-01-30 02:46:30 +0300

    Попытка №59254273450

--- dumps_any_run.py
+++ dumps_any_run.py
@@ -4,14 +4,13 @@ from bs4 import BeautifulSoup, GuessedAtParserWarning
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 import time
-import getpass
 
 
 class Downloader:
 
     def __init__(self):
         self._driver = webdriver.Firefox()
-        self._driver.set_page_load_timeout(13)
+        self._driver.set_page_load_timeout(15)
 
     @staticmethod
     def open_main_page(driver: selenium.webdriver.Firefox):
@@ -27,8 +26,8 @@ class Downloader:
         button1 = driver.find_element(By.XPATH, value="//li[@title='Sign In']")
         button1.click()
         login = input("Пожалуйста, введите логин от any run - ")
-        password = getpass.getpass("Пожалйста, введите пароль от any run - ")
         driver.find_element(by=By.ID, value='at-field-username_and_email').send_keys(login)
+        password = input('Пожалйста, введите пароль от any run - ')
         driver.find_element(by=By.ID, value='at-field-password').send_keys(password)
         button_sign = driver.find_element(by=By.CSS_SELECTOR, value='button.at-btn')
         button_sign.click()
@@ -47,15 +46,16 @@ class Downloader:
 
     @staticmethod
     def download_pcaps(driver: selenium.webdriver.Firefox, links):
-        try:
-            driver.get('https://app.any.run' + links[0]['link'])
-        except TimeoutException as e:
-            print(e)
-        except GuessedAtParserWarning as e:
-            print(e)
-        time.sleep(2)
-        button_pcap = driver.find_element(by=By.ID, value='pcapLi')
-        button_pcap.click()
+        for i in range(len(links)):
+            try:
+                driver.get('https://app.any.run' + links[i]['link'])
+            except TimeoutException as e:
+                print(e)
+            except GuessedAtParserWarning as e:
+                print(e)
+            button_pcap = driver.find_element(by=By.ID, value='pcapLi')
+            button_pcap.click()
+            time.sleep(3)
 
     def run(self):
         self.open_main_page(driver=self._driver)
